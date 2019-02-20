a69d3444fb11c3ed7cb8af26dae6c8e652116c7c
A.Munoz <A-Munoz@users.noreply.github.com>
2019-02-20 17:43:19 -0500

    file update: css, main component

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-//import Home from "@/components/home";
+import Recorder from "@/components/recordingComponent.vue";
 
 Vue.use(Router);
 
@@ -15,14 +15,15 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/recorder",
+      name: "recorder",
+      component: Recorder
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: function() {
-        return import(/* webpackChunkName: "about" */ "./views/About.vue");
-      }
+      // component: function() {
+      //   return import(/* webpackChunkName: "about" */ "./views/About.vue");
+      // }
     }
   ]
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <recordComp msg="Lets create a record button and save"/>
-  </div>
+    <div class="home">
+        <div class="header"></div>
+        <div id="record">
+            <div id="outer"></div>
+            <div id="middle"></div>
+            <div id="center"></div>
+        </div>
+        <div class="logo"></div>
+        <Main/>
+
+    </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import recordComp from "@/components/recordingComponent.vue";
+    'use strict'
+    // @ is an alias to /src
+    import jump from 'jump.js'
+    import Recording from '@/components/recordingComponent.vue'
+    import Main from '@/components/main.vue'
+    
+    export default {
+        name: 'home',
+        components: {
+            Main
+        },
+        methods: {
+            move() {
+                jump('.audio',{ duration: 1500})
+            },
+            callRecord() {
+                Main
+            }
+           
+        }
+        
+      
+    }
+    
 
-export default {
-  name: "home",
-  components: {
-    recordComp
-  }
-};
+    
+
 </script>
