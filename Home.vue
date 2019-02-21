33dfe41b0af81942da78c136324194a1db91eabd
PlatFleece <maverick.alpha4@gmail.com>
2019-02-21 08:44:23 -0500

    Playback

---
+++ Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <recordComp msg="Lets create a record button and save"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import recordComp from "@/components/recordingComponent.vue";
+
+export default {
+  name: "home",
+  components: {
+    recordComp
+  }
+};
+</script>
