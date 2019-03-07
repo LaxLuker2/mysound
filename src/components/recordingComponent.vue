c2451e332a78faa16d42a48ecbc195367964a013
Luke Schwarting <jeluker2@gmail.com>
2019-03-07 15:31:27 -0500

    testing recording with alerts for iphone

--- src/components/recordingComponent.vue
+++ src/components/recordingComponent.vue
@@ -20,92 +20,74 @@ export default {
   },
   methods: {
     record() {
-      navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
-        if ($(".rec").hasClass("Record")) {
-          mediaRecorder = new MediaRecorder(stream);
-          mediaRecorder.start(250);
-          console.log(mediaRecorder.state);
-          console.log("recorder started");
-          $(".rec").css("background-color", "red");
-          $(".rec").html("Recording");
-          $(".rec").addClass("Recording");
-          $(".rec").removeClass("Record");
+      alert("rec btn pressed");
+      // if (
+      //   !navigator.mediaDevices.getUserMedia ||
+      //   navigator.mediaDevices.getUserMedia
+      // ) {
+      //   navigator.mediaDevices.getUserMedia =
+      //     navigator.mediaDevices.getUserMedia ||
+      //     navigator.mediaDevices.webkitGetUserMedia ||
+      //     navigator.mediaDevices.mozGetUserMedia ||
+      //     navigator.mediaDevices.msGetUserMedia;
 
-          chunks = [];
+      //   alert("navigator.mediaDevices.getUserMedia");
+      //   alert(navigator.mediaDevices.getUserMedia);
+      // }
 
-          mediaRecorder.ondataavailable = function(e) {
-            //console.log('Data available...');
-            //console.log(e.data);
-            console.dir(e);
+      try {
+        alert("in try");
+        navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+          if ($(".rec").hasClass("Record")) {
+            mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.start(250);
+            console.log(mediaRecorder.state);
+            console.log("recorder started");
+            alert("recorder started");
+            $(".rec").css("background-color", "red");
+            $(".rec").html("Recording");
+            $(".rec").addClass("Recording");
+            $(".rec").removeClass("Record");
 
-            chunks.push(e.data);
+            chunks = [];
+
+            mediaRecorder.ondataavailable = function(e) {
+              //console.log('Data available...');
+              //console.log(e.data);
+              console.dir(e);
+
+              chunks.push(e.data);
+            };
+          } else {
+            mediaRecorder.stop();
+            console.log(mediaRecorder.state);
+            console.log("recorder stopped");
+            alert("recorder stopped");
+            $(".rec").css("background-color", "blue");
+            $(".rec").html("Record");
+            $(".rec").removeClass("Recording");
+            $(".rec").addClass("Record");
+          }
+          mediaRecorder.onstop = function(e) {
+            console.log("recorder stopped");
+
+            var blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+            chunks = [];
+            var audioURL = window.URL.createObjectURL(blob);
+            //audio.src = audioURL;
+
+            $(".downloadLink").attr("href", audioURL);
+
+            var rand = Math.floor(Math.random() * 10000000);
+            var name = "mysound_" + rand + ".mp3";
+            $(".downloadLink").attr("download", name);
+            $(".downloadLink").attr("name", name);
           };
-        } else {
-          mediaRecorder.stop();
-          console.log(mediaRecorder.state);
-          console.log("recorder stopped");
-          $(".rec").css("background-color", "blue");
-          $(".rec").html("Record");
-          $(".rec").removeClass("Recording");
-          $(".rec").addClass("Record");
-
-          // blob = new Blob(chunks, { type: "video/mpeg" });
-          // //blob = new Blob(chunks, { type: "video/webm" });
-
-          // var audioURL = window.URL.createObjectURL(blob);
-          // //console.dir(blob);
-
-          // $(".downloadLink").attr("href", audioURL);
-          // //$('.playback').attr("src", audioURL);
-          // //videoElement.src = audioURL;
-          // //downloadLink.innerHTML = 'Download audio file';
-
-          // var rand = Math.floor(Math.random() * 10000000);
-          // var name = "mysound_" + rand + ".mp3";
-
-          // //$(".playback").attr("src", audioURL);
-
-          // $(".downloadLink").attr("download", name);
-          // $(".downloadLink").attr("name", name);
-        }
-        mediaRecorder.onstop = function(e) {
-          console.log("recorder stopped");
-
-          // var clipName = prompt("Enter a name for your sound clip");
-
-          // var clipContainer = document.createElement("section");
-          // var clipLabel = document.createElement("p");
-          // var audio = document.createElement("audio");
-          // var deleteButton = document.createElement("button");
-
-          // clipContainer.classList.add("clip");
-          // audio.setAttribute("controls", "");
-          // deleteButton.innerHTML = "Delete";
-          // clipLabel.innerHTML = clipName;
-
-          // clipContainer.appendChild(audio);
-          // clipContainer.appendChild(clipLabel);
-          // clipContainer.appendChild(deleteButton);
-          // soundClips.appendChild(clipContainer);
-
-          var blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
-          chunks = [];
-          var audioURL = window.URL.createObjectURL(blob);
-          //audio.src = audioURL;
-
-          $(".downloadLink").attr("href", audioURL);
-
-          var rand = Math.floor(Math.random() * 10000000);
-          var name = "mysound_" + rand + ".mp3";
-          $(".downloadLink").attr("download", name);
-          $(".downloadLink").attr("name", name);
-
-          // deleteButton.onclick = function(e) {
-          //   var evtTgt = e.target;
-          //   evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-          // };
-        };
-      });
+        });
+      } catch (e) {
+        alert("err");
+        alert(e);
+      }
     }
   }
 };
@@ -129,8 +111,5 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
